335b51b70c965db8d57959d273accf962948cfa7
David Antuña <davidanrod@gmail.com>
2018-05-10 15:54:15 +0200

    Add files via upload

---
+++ code/main.js
@@ -0,0 +1,75 @@
+const game = function () {
+  ////////// Load Quintus object //////////
+  const Q = (window.Q = Quintus()
+    .include('Sprites, Scenes, Input, UI, Touch, TMX, Anim, 2D, Audio')
+    .setup({
+      width: 320,
+      height: 480,
+    })
+    .controls()
+    .touch());
+
+  ////////// Player Sprite //////////
+  Q.Sprite.extend('Player', {
+    init: function (p) {
+      this._super(p, {
+        sprite: 'walk',
+        sheet: 'walk_up',
+        direction: 'up',
+        x: 150,
+        y: 380,
+        dead: false,
+        health: 3,
+        stepDistance: 16,
+        stepDelay: 0.2
+      });
+
+      this.add('2d, stepControls, animation');
+
+      this.on('hit.sprite', function (collision) {
+        //TODO
+      });
+    },
+
+    step: function (dt) {
+
+    },
+
+    hit: function (dmg) {
+      this.p.health -= dmg;
+      if (this.p.health <= 0) {
+        this.p.dead = true
+      }
+    },
+  });
+
+
+  ////////// ENEMIES //////////
+
+  //// Default component ////
+  Q.component('defaultEnemy', {
+    added: function () {
+      this.entity.on('bump.left,bump.right,bump.bottom,bump.top', function (collision) {
+        if (collision.obj.isA('Player')) {
+          collision.obj.hit(1);
+          console.log('whops')
+        }
+      });
+    },
+  });
+
+  ////////// Load TMX level //////////
+  Q.scene('test', function (stage) {
+    Q.stageTMX('prueba2.tmx', stage);
+  });
+
+  Q.load(
+    'walk.png, walk.json',
+    function () {
+      Q.compileSheets('walk.png', 'walk.json');
+      Q.loadTMX('prueba2.tmx', function () {
+        Q.stageScene('test');
+      });
+    }
+  );
+};
---
+++ data/tiles.tsx
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tiles" tilewidth="16" tileheight="16" tilecount="1600" columns="40" backgroundcolor="#898989">
+ <image source="../Desktop/tiles.png" width="640" height="640"/>
+ <tile id="859">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="860">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="899">
+  <objectgroup draworder="index">
+   <object id="8" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="900">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="984">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1027">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1064">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+</tileset>
